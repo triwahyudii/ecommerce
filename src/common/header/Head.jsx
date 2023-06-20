432009c954aac3c6243c18ccf9939913b10db2da
tri wahyudi <mesirah6@gmail.com>
2023-06-20 22:58:37 +0700

    styling

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
+
 const Head = () => {
     return (
         <>
-        <h1>Head</h1>
+        <section className="head">
+            <div className="container d_flex">
+                <div className="left row">
+                    <i className="fa fa-phone"></i>
+                    <label>+62857 4537 7009</label>
+                    <i className="fa fa-envelope"></i>
+                    <label>developer@gmail.com</label>
+                </div>
+                <div>
+                    <label>Theme FQA</label>
+                    <label>Need Help</label>
+                    <span>🏁</span>
+                    <label htmlFor="">EN</label>
+                    <span>🏁</span>
+                    <label htmlFor="">USD</label>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
