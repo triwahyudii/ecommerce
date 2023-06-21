7b1b441b28dacd092857716f85b84f2b1e0688c2
tri wahyudi <mesirah6@gmail.com>
2023-06-21 14:08:24 +0700

    add: css

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
+
+
 
 const Navbar = () => {
+    
+    const [MobileMenu, setMobileMenu] = useState(false)
+
     return (
         <>
-        <h2>Navbar</h2>
+            <Router>
+                <header className="header">
+                    <div className="container d_flex">
+                        <div className="categories d_flex">
+                            <span className="fa-solid fa-border-all"></span>
+                            <h4>
+                                Categories <i className="fa fa-chevron-down"></i>
+                            </h4>
+                        </div>
+                        <div className="navlink">
+                            <ul className="nav">
+                                <li>
+                                    <Link to="/">home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/pages">pages</Link>
+                                </li>
+                                <li>
+                                    <Link to="/user">user account</Link>
+                                </li>
+                                <li>
+                                    <Link to="/vendor">vendor account</Link>
+                                </li>
+                                <li>
+                                    <Link to="/track">track my order</Link>
+                                </li>
+                                <li>
+                                    <Link to="contact">contact</Link>
+                                </li>
+                            </ul>
+                            <button className="toggle" onClick={()=> setMobileMenu(!MobileMenu)}>
+                                {MobileMenu ? <i className="fas fa-times close home-bth"></i> : 
+                                <i className="fas fa-bars open"></i>}
+                            </button>
+                        </div>
+                    </div>
+                </header>
+            </Router>
         </>
     )
 }
