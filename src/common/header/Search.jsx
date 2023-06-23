329ac1bdece457ef2cbc2f0c2472605929168f0b
tri wahyudi <mesirah6@gmail.com>
2023-06-23 11:23:45 +0700

    fix

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../assets/Logo.png"
+import logo from "../../assets/Logo.png";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const Search = () => {
